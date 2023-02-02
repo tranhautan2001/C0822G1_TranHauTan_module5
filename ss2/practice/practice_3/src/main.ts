5a684554ed149c454275ed4a5068bffac4d136d6
unknown <tranhautanvlog.vn@gmail.com>
2023-02-02 15:44:00 +0700

    n

---
+++ ss2/practice/practice_3/src/main.ts
@@ -0,0 +1,10 @@
+function fibonacci( num:number): number {
+    if (num <= 1) return num;
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+let sum : number = 0;
+// let num:number = + prompt("nhập số fibonacci");
+// for (let i = 0; i <num ; i++) {
+//     sum += fibonacci(i)
+// }
+// console.log(" tổng " + num + " số fibonacci là :" + sum)

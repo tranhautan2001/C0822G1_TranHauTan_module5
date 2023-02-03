01c3e7ef45a6df0de9222813451967275add0cc8
unknown <tranhautanvlog.vn@gmail.com>
2023-02-03 13:24:30 +0700

    n

--- ss2/exeris/exercis fibonacci/src/main.ts
+++ ss2/exeris/exercis fibonacci/src/main.ts
@@ -1,10 +1,21 @@
+
 function fibonacci( num:number): number {
+
     if (num <= 1) return num;
+
     return fibonacci(num - 1) + fibonacci(num - 2);
 }
+
 let sum : number = 0;
+
+// @ts-ignore
+
 let num:number = + prompt("nhập số fibonacci");
+
  for (let i = 0; i <num ; i++) {
+
     sum += fibonacci(i)
+
 }
+
 console.log(" tổng " + num + " số fibonacci là :" + sum)
---
+++ ss3/practice/practice2/angular-change-font-size/src/app/font-size-editor/font-size-editor.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-font-size-editor',
+  templateUrl: './font-size-editor.component.html',
+  styleUrls: ['./font-size-editor.component.css']
+})
+
+export class FontSizeEditorComponent implements OnInit {
+  changeFontSizeValue(fontSize) {
+    this.fontSize = fontSize;
+  }
+  fontSize = 14;
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
+
